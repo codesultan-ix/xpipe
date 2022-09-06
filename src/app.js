bac1acca8ad4d847f756aa608005571a1f04254c
codesultan <codesultan369@gmail.com>
2022-09-06 17:20:58 +0100

    test pipline

--- src/app.js
+++ src/app.js
@@ -4,4 +4,8 @@ const app = express();
 app.get("/test", (_req, res) =>  {
   res.status(200).send("Hello world")
 })
+
+app.get("/", (_req, res) =>  {
+    res.status(200).send("Hello world pipline")
+  })
 module.exports = app;
